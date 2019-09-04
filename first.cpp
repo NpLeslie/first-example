87642f918222d70b1a8333200b549378ea44e8d8
Nathaniel Leslie <npleslie@mail.csuchico.edu>
2019-09-04 10:37:48 -0700

    changed outpit text to Hello World! added note with next three lyrics

--- first.cpp
+++ first.cpp
@@ -3,7 +3,9 @@
 
 int main()
 {
-  std::cout<<"Hi."<<std::endl;
-
+std::cout<<"Hello World!"<<std::endl;
+/*Programed to work and not to feel!
+ Not even sure if this is real...
+ Hello world!*/
   return 0;
 }
